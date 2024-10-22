2cb3eaf01863797413b64c522403c63e016ca8e1
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-10-23 01:48:47 +0900

    refactor: use char code offset to sum

--- Sum-strings-as-numbers-4kyu/sum_strings.py
+++ Sum-strings-as-numbers-4kyu/sum_strings.py
@@ -1,72 +1,7 @@
 from typing import Tuple
 
-SumTable = {
-    ## 0 group
-    "00": ("0", False),
-    "01": ("1", False),
-    "02": ("2", False),
-    "03": ("3", False),
-    "04": ("4", False),
-    "05": ("5", False),
-    "06": ("6", False),
-    "07": ("7", False),
-    "08": ("8", False),
-    "09": ("9", False),
-    ## 1 group
-    "11": ("2", False),
-    "12": ("3", False),
-    "13": ("4", False),
-    "14": ("5", False),
-    "15": ("6", False),
-    "16": ("7", False),
-    "17": ("8", False),
-    "18": ("9", False),
-    "19": ("0", True),
-    ## 2 group
-    "22": ("4", False),
-    "23": ("5", False),
-    "24": ("6", False),
-    "25": ("7", False),
-    "26": ("8", False),
-    "27": ("9", False),
-    "28": ("0", True),
-    "29": ("1", True),
-    ## 3 group
-    "33": ("6", False),
-    "34": ("7", False),
-    "35": ("8", False),
-    "36": ("9", False),
-    "37": ("0", True),
-    "38": ("1", True),
-    "39": ("2", True),
-    ## 4 group
-    "44": ("8", False),
-    "45": ("9", False),
-    "46": ("0", True),
-    "47": ("1", True),
-    "48": ("2", True),
-    "49": ("3", True),
-    ## 5 group
-    "55": ("0", True),
-    "56": ("1", True),
-    "57": ("2", True),
-    "58": ("3", True),
-    "59": ("4", True),
-    ## 6 group
-    "66": ("2", True),
-    "67": ("3", True),
-    "68": ("4", True),
-    "69": ("5", True),
-    ## 7 group
-    "77": ("4", True),
-    "78": ("5", True),
-    "79": ("6", True),
-    ## 8 group
-    "88": ("6", True),
-    "89": ("7", True),
-    ## 9 group
-    "99": ("8", True),
-}
+UNICODE0 = 48
+UNICODE9 = 57
 
 
 def sum_strings(x: str, y: str) -> str:
@@ -98,18 +33,13 @@ def sum_strings(x: str, y: str) -> str:
 
 
 def SumDigit(a: str, b: str, remainder: bool) -> Tuple[str, bool]:
-    if (a + b) in SumTable:
-        tempSum, newremainer = SumTable[a + b]
+    sumCharCode = ord(a) + ord(b) - UNICODE0 + remainder
+    if sumCharCode > UNICODE9:
+        remainder = True
+        sumCharCode -= 10
     else:
-        tempSum, newremainer = SumTable[b + a]
-    if remainder:
-        if ("1" + tempSum) in SumTable:
-            sum, returnRemainder = SumTable["1" + tempSum]
-        else:
-            sum, returnRemainder = SumTable[tempSum + "1"]
-        return sum, returnRemainder or newremainer
-    else:
-        return tempSum, newremainer
+        remainder = False
+    return chr(sumCharCode), remainder
 
 
 def PreProcessing(input: str) -> str:
